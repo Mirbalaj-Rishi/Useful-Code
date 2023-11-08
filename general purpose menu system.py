3aac359e39884231d49b1077ca58c98e1062c235
Mirbalaj-Rishi <150295174+Mirbalaj-Rishi@users.noreply.github.com>
2023-11-08 15:50:38 -0500

    Add files via upload

---
+++ general purpose menu system.py
@@ -0,0 +1,29 @@
+# general purpose menu system
+
+def mainMenu(Option1, Option2, Option3):
+    goodChoice = "no"
+    while goodChoice == "no":
+        userChoice = input(f"""
+Please Input Your Choice
+1.{Option1}
+2.{Option2}
+3.{Option3}
+
+""").lower()
+    
+        print()
+        if userChoice == "1" or userChoice == Option1.lower():
+            goodChoice = "yes"
+            return Option1
+        elif userChoice == "2" or userChoice == Option2.lower():
+            goodChoice = "yes"
+            return Option2
+        elif userChoice == "3" or userChoice == Option3.lower():
+            goodChoice = "yes"
+            return Option3
+        else:
+            goodChoice = "no"
+            print("please choose one of the options below")
+
+
+print(mainMenu("first", "second", "third"))
