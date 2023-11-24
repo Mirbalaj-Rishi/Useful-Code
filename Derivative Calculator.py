f822d7f88229162197fda55cd0b9f29ba27232f3
Mirbalaj Rishi <150295174+Mirbalaj-Rishi@users.noreply.github.com>
2023-11-24 18:32:58 -0500

    Update Derivative Calculator.py
    This version can now handle any function of the form numbervariable^exponet (for example 3x^2). Further bug fixes are coming in the next version.

--- Derivative Calculator.py
+++ Derivative Calculator.py
@@ -1,22 +1,13 @@
 print("This program is made by Mirbalaj Rishi")
 print("")
 
+# This collects the user's inputs
 userInput = input("Please, input your function: ")
-list = []
-constant = ""
-variable = ""
-letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "m", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
-"""
-def numbercheck(n):
-    try:
-        x = int(n)
-        return int(n)
-    except:
-        x = n
-"""
 
 
+
+# this funion seperates the exponent in the users input by looking for a "^"
+# character and combining everything after it into a string
 def exponetCheck(e):
     exponet = ""
     if e.find("^") != -1:
@@ -24,19 +15,22 @@ def exponetCheck(e):
         for i in range(l+1,len(e)):
             exponet += e[i]
     return exponet
-        
-
+         
+# this function separates the numbers in a function by combining all integers that are before the ^ sign
 def numberCheck2(n):
     number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     num = ""
-    for i in n: 
-        if i in number:
+    for i in n:
+        if i == "^" or i == "":
+            break
+        elif i in number:
             num += i
     if num !="":
         return num
     else:
         return "no"
-        
+
+# this function seperates the variable 
 def letterCheck(l):
     letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "m", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     let = ""
@@ -48,25 +42,19 @@ def letterCheck(l):
     else:
         return "no"
         
-def symbolsCheck(i):
-    if numberCheck2(i)!= "no":
-        return numberCheck2(i)
-    elif letterCheck(i)!="no":
-        return letterCheck(i)
-    else:
-        return "nothing"
+
     
 
+#this takes the number and exponet, and turns them integers to calculated the derivative
+exponet = int(exponetCheck(userInput))
+number = int(numberCheck2(userInput))
+variable = letterCheck(userInput)
+totalNumber = exponet*number
+newExponet = exponet - 1
 
+#this prints out the final derivative
+print(str(totalNumber)+variable+"^"+str(newExponet))
 
-orginialList = []
-
-print(exponetCheck(userInput))
-
-list.append(numberCheck2(userInput))
-list.append(letterCheck(userInput))
-
-print(numberCheck2(userInput))
 
 
 
