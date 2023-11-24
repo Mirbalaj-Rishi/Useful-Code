d72f67771354bf2126169d2da40e863da61ddb00
Mirbalaj Rishi <150295174+Mirbalaj-Rishi@users.noreply.github.com>
2023-11-24 13:45:49 -0500

    Update Derivative Calculator.py
    Added an exponent check function that can separate exponents after the "^" symbol. For example, the exponent check function will read an input of 200^20 and return 20. The next update will allow the program to multiply the exponent with the number.

--- Derivative Calculator.py
+++ Derivative Calculator.py
@@ -15,11 +15,24 @@ def numbercheck(n):
     except:
         x = n
 """
+
+
+def exponetCheck(e):
+    exponet = ""
+    if e.find("^") != -1:
+        l = e.find("^")
+        for i in range(l+1,len(e)):
+            exponet += e[i]
+    return exponet
+        
+
 def numberCheck2(n):
     number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
     num = ""
-    if n in number:
-        num += n
+    for i in n: 
+        if i in number:
+            num += i
+    if num !="":
         return num
     else:
         return "no"
@@ -27,8 +40,10 @@ def numberCheck2(n):
 def letterCheck(l):
     letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "m", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     let = ""
-    if l in letter:
-        let += l
+    for i in l:
+        if i in letter:
+            let += i
+    if let !="":
         return let
     else:
         return "no"
@@ -46,14 +61,12 @@ def symbolsCheck(i):
 
 orginialList = []
 
-for i in userInput:
-    list.append(symbolsCheck(i))
+print(exponetCheck(userInput))
 
-print(list)
-for i in list:
-    if i in number:
-        variable = int(i)
-    if i in letter:
-        constant = 1
-print(variable*constant)
+list.append(numberCheck2(userInput))
+list.append(letterCheck(userInput))
+
+print(numberCheck2(userInput))
+
+
 
